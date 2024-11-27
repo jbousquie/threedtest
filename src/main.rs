57c5e5b2b9d05d516b6d47e76e3e54338e88c9f9
jerome <jerome.bousquie@rodez.fr>
2024-11-27 16:49:07 +0100

    cylind

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+// https://github.com/asny/three-d/blob/master/examples/shapes/src/main.rs
+
+use three_d::*;
+
+pub fn main() {
+    let window = Window::new(WindowSettings {
+        title: "Test ThreeD".to_string(),
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
+
+    let context = window.gl();
+
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        vec3(5.0, 2.0, 2.5),
+         vec3(0.0, 0.0, -0.5),
+         vec3(0.0, 1.0, 0.0),
+         degrees(45.0),
+         0.1,
+         1000.0,
+    );
+
+    let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
+
+    let mut sphere = Gm::new(
+        Mesh::new(&context, &CpuMesh::sphere(16)),
+        PhysicalMaterial::new_transparent(
+            &context, 
+            &CpuMaterial {
+                albedo: Srgba {
+                    r: 255,
+                    g: 0,
+                    b: 0,
+                    a: 200,
+                },
+                ..Default::default()
+            },
+        ),
+    );
+    sphere.set_transformation(Mat4::from_translation(vec3(0.0, 1.3, 0.0)) * Mat4::from_scale(0.2));
+
+    let cylinder = Gm::new(
+        Mesh::new(&context, &CpuMesh::cylinder(16)),
+          PhysicalMaterial::new_transparent(
+            &context,
+            &CpuMaterial {
+                albedo: Srgba {
+                    r: 0,
+                    g: 255,
+                    b: 0,
+                    a: 200,
+                },
+                ..Default::default()
+            },
+        ),
+    );
+
+
+    let light0: DirectionalLight = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, -0.5, -0.5));
+    let light1: DirectionalLight = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, 0.5, 0.5));
+
+
+    window.render_loop(move | mut frame_input | {
+        camera.set_viewport(frame_input.viewport);
+        control.handle_events(&mut camera, &mut frame_input.events);
+
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            .render(
+                &camera,
+                &sphere,
+                &[&light0, &light1],
+            );
+
+        FrameOutput::default()
+    });
+    
+
 }
