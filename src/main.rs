94d9f7c8d67aaa3ba969c6a176051fad99f29d46
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2024-12-02 16:41:57 +0100

    animation par instance

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // https://github.com/asny/three-d/blob/master/examples/instanced_shapes/src/main.rs
 
+// https://docs.rs/three-d/latest/three_d/
+
 use three_d::*;
 
 pub fn main() {
@@ -43,31 +45,8 @@ pub fn main() {
 
 
     let count = side_count * side_count * side_count;
-    instanced_mesh.set_instances(&Instances {
-        transformations: (0..count)
-            .map(|i| {
-                let x = i % side_count;
-                let y = (i / side_count) % side_count;
-                let z = i / (side_count * side_count);
-            
-                Mat4::from_translation(vec3(3.0 * x as f32 - 1.5 * side_count as f32, 3.0 * y as f32 - 1.5 * side_count as f32, 3.0 * z as f32 - 1.5 * side_count as f32))
-            })
-            .collect(),
-        colors: Some(
-            (0..count)
-            .map(|i| {
-                let x = i % side_count;
-                let y = (i / side_count) % side_count;
-                let z = i / (side_count * side_count);
-                let r = (x as f32 / side_count as f32 * 255.0) as u8 + 26;
-                let g = (y as f32 / side_count as f32 * 255.0) as u8 + 26;
-                let b = (z as f32 / side_count as f32 * 255.0) as u8 + 26;
-                Srgba::new(r, g, b, 255)
-            })
-            .collect(),
-        ),
-        ..Instances::default()
-    });
+    let instances = compute_instances(count, side_count, 0.0);
+    instanced_mesh.set_instances(&instances);
         
 
     window.render_loop(move | mut frame_input | {
@@ -75,19 +54,55 @@ pub fn main() {
         control.handle_events(&mut camera, &mut frame_input.events);
 
 
+        let time = (frame_input.accumulated_time * 0.001) as f32;
+        let instances = compute_instances(count, side_count, time);
+        instanced_mesh.set_instances(&instances);
 
 
-        frame_input
-            .screen()
-            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
-            .render(
+        let screen = frame_input.screen();
+        screen.clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0));
+        screen.render(
                 &camera,
                 &instanced_mesh,
                 &[&light0, &light1],
             );
-
+        println!("{}", frame_input.elapsed_time);
         FrameOutput::default()
     });
     
 
 }
+
+fn compute_instances(count: i32, side_count: i32, time: f32) -> Instances {
+    Instances {
+        transformations: (0..count)
+            .map(|i| {
+                let x = i % side_count;
+                let y = (i / side_count) % side_count;
+                let z = i / (side_count * side_count);
+            
+                let translation = Mat4::from_translation(vec3(3.0 * x as f32 - 1.5 * side_count as f32, 3.0 * y as f32 - 1.5 * side_count as f32, 3.0 * z as f32 - 1.5 * side_count as f32));
+                let rotation = 
+                    Mat4::from_angle_x(radians(time * x as f32 * 0.3)) *
+                    Mat4::from_angle_y(radians(time * y as f32 * 0.2)) *
+                    Mat4::from_angle_z(radians(time * z as f32 * 0.1));
+            
+                translation * rotation
+                })
+            .collect(),
+        colors: Some(
+            (0..count)
+            .map(|i| {
+                let x = i % side_count;
+                let y = (i / side_count) % side_count;
+                let z = i / (side_count * side_count);
+                let r = (x as f32 / side_count as f32 * 255.0) as u8 ;
+                let g = (y as f32 / side_count as f32 * 255.0) as u8;
+                let b = (z as f32 / side_count as f32 * 255.0) as u8;
+                Srgba::new(r, g, b, 255)
+            })
+            .collect(),
+        ),
+        ..Instances::default()
+    } 
+}  
